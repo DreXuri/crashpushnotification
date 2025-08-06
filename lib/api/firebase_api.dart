b5d38cc2805951a67390418f26a62377a4cc2f71
DreXuri <balogundamilaree@gmail.com>
2025-08-06 09:09:50 +0100

    firebase added

---
+++ lib/api/firebase_api.dart
@@ -0,0 +1,15 @@
+class FirebaseApi {
+  //instance of FirebaseApi messaging
+  FirebaseApi._privateConstructor();
+
+
+
+
+  //// Initialize Firebase notification
+
+  ///function for recieved messaging
+  
+  ///function for initializing foregrame and background messaging
+
+  
+}
