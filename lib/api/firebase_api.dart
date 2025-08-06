205563455fe173af8271c4b97c2232bbed0ae908
DreXuri <balogundamilaree@gmail.com>
2025-08-06 09:34:53 +0100

    implement firebase token service

--- lib/api/firebase_api.dart
+++ lib/api/firebase_api.dart
@@ -1,15 +1,29 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
 class FirebaseApi {
   //instance of FirebaseApi messaging
-  FirebaseApi._privateConstructor();
-
-
-
+final _firebaseMessaging = FirebaseMessaging.instance;
+ 
 
   //// Initialize Firebase notification
+Future<void> initNotifications() async {
 
-  ///function for recieved messaging
+//request from user(promt user)
+await _firebaseMessaging.requestPermission();
+
+
+//fetch the token from device
+String? fbmToken = await _firebaseMessaging.getToken();
+
+//print token (intially you ll send this to your device
+print("Firebase Messaging Token: $fbmToken");
+ 
+}
+
+  ///function for recieved messaging handle
   
-  ///function for initializing foregrame and background messaging
 
+
+//function to handle foreground and background messaging setting
   
 }
